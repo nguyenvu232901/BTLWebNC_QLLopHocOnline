477275a4af87081d72ca63341d77a7c497b53b1a
nguyenvu232901 <nguyentuanvu2002hn@gmail.com>
2024-07-22 15:31:54 +0700

    Add project files.

---
+++ BTLWebNC_QLLopHocOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BTLWebNC_QLLopHocOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
